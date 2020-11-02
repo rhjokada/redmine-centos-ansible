8c62f5c494b066ce4653114b82c5d733451b61b3
rhjokada <71449049+rhjokada@users.noreply.github.com>
2020-11-02 18:12:07 +0900

    make puma dir

--- roles/redmine-install/templates/puma.rb
+++ roles/redmine-install/templates/puma.rb
@@ -2,8 +2,8 @@ app_root = '{{ redmine_dir }}'
 
 directory app_root
 environment 'production'
-pidfile "#{app_root}/tmp/pids/puma.pid"
-state_path "#{app_root}/tmp/pids/puma.state"
+pidfile "#{app_root}/tmp/puma/puma.pid"
+state_path "#{app_root}/tmp/puma/puma.state"
 bind "unix://#{app_root}/tmp/sockets/redmine.sock"
 activate_control_app
 
