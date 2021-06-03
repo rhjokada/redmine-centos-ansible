dd2385124eae36880bab1c1e80ac3d5a851d6e3d
rhjokada <71449049+rhjokada@users.noreply.github.com>
2021-06-03 09:58:29 +0900

    Update puma.rb

--- roles/redmine-install/templates/puma.rb
+++ roles/redmine-install/templates/puma.rb
@@ -8,3 +8,9 @@ bind "unix://#{app_root}/tmp/sockets/redmine.sock"
 activate_control_app
 
 stdout_redirect "#{app_root}/puma.stdout.log", "#{app_root}/puma.stderr.log"
+
+# threads
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+# workers
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
